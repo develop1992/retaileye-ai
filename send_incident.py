73f43ba9ef76e870a564786aaab860db5dddb14a
Bahram Afsharipoor <bahram@barbarians.com>
2025-04-18 17:45:59 -0400

    record via ivcam streaming

--- send_incident.py
+++ send_incident.py
@@ -2,7 +2,7 @@ import requests
 
 def send_incident(incidents):
     try:
-        response = requests.post("https://2b2e-47-19-81-210.ngrok-free.app/retaileye/incidents/bulk", json=incidents, timeout=30)
+        response = requests.post("https://d700-47-19-81-210.ngrok-free.app/retaileye/incidents/bulk", json=incidents, timeout=30)
         response.raise_for_status()
         print(f"\n {len(incidents)} incidents sent in bulk.")
     except Exception as e:
